3f65c8fdeddd95c80f4661dee056fb0f7b2d2037
kkanapuram <kkanapuram@ebay.com>
2016-08-21 14:34:28 -0700

    initial commit

--- public/js/ngControllers/camphorSvc.js
+++
@@ -1,5 +0,0 @@
-angular.module("camphorScent")
-    .service('camphorSvc',['$http','$q','$window',function($http,$q,$window) {
-    var me = this;
-    return '';
-}])
--- public/js/ngControllers/headerCtrl.js
+++
@@ -1,7 +0,0 @@
-angular.module("camphorScent")
-
-    .controller('headerCtrl', function ($scope,camphorSvc,$window) {
-
-    var me = camphorSvc;
-
-});
